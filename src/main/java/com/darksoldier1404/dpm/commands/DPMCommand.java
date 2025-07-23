07fb2293e92fd8effbaf72b06548ad3735eaa4f3
darksoldier1404 <darksoldier1029@gmail.com>
2025-07-23 23:34:40 +0900

    fix command permission _ to .

--- src/main/java/com/darksoldier1404/dpm/commands/DPMCommand.java
+++ src/main/java/com/darksoldier1404/dpm/commands/DPMCommand.java
@@ -22,39 +22,39 @@ public class DPMCommand {
         lang = plugin.data.getLang();
         builder = new CommandBuilder(plugin.data.getPrefix());
 
-        builder.addSubCommand("open", "dpm_use", lang.get("help_open"), true, (p, args) -> {
+        builder.addSubCommand("open", "dpm.use", lang.get("help_open"), true, (p, args) -> {
             if (args.length == 2) DPMFunction.openMenu((Player) p, args[1]);
             else p.sendMessage(prefix + lang.get("help_open"));
         });
-        builder.addSubCommand("create", "dpm_admin", lang.get("help_create"), true, (p, args) -> {
+        builder.addSubCommand("create", "dpm.admin", lang.get("help_create"), true, (p, args) -> {
             if (args.length == 3) DPMFunction.createMenu((Player) p, args[1], args[2]);
             else p.sendMessage(prefix + lang.get("help_create"));
         });
-        builder.addSubCommand("delete", "dpm_admin", lang.get("help_delete"), true, (p, args) -> {
+        builder.addSubCommand("delete", "dpm.admin", lang.get("help_delete"), true, (p, args) -> {
             if (args.length == 2) DPMFunction.deleteMenu((Player) p, args[1]);
             else p.sendMessage(prefix + lang.get("help_delete"));
         });
-        builder.addSubCommand("title", "dpm_admin", lang.get("help_title"), true, (p, args) -> {
+        builder.addSubCommand("title", "dpm.admin", lang.get("help_title"), true, (p, args) -> {
             if (args.length >= 3) DPMFunction.setTitle((Player) p, args[1], args);
             else p.sendMessage(prefix + lang.get("help_title"));
         });
-        builder.addSubCommand("row", "dpm_admin", lang.get("help_row"), true, (p, args) -> {
+        builder.addSubCommand("row", "dpm.admin", lang.get("help_row"), true, (p, args) -> {
             if (args.length == 3) DPMFunction.setRow((Player) p, args[1], args[2]);
             else p.sendMessage(prefix + lang.get("help_row"));
         });
-        builder.addSubCommand("items", "dpm_admin", lang.get("help_items"), true, (p, args) -> {
+        builder.addSubCommand("items", "dpm.admin", lang.get("help_items"), true, (p, args) -> {
             if (args.length == 2) DPMFunction.openItemSettingGUI((Player) p, args[1]);
             else p.sendMessage(prefix + lang.get("help_items"));
         });
-        builder.addSubCommand("price", "dpm_admin", lang.get("help_price"), true, (p, args) -> {
+        builder.addSubCommand("price", "dpm.admin", lang.get("help_price"), true, (p, args) -> {
             if (args.length == 2) DPMFunction.openPriceSettingGUI((Player) p, args[1]);
             else p.sendMessage(prefix + lang.get("help_price"));
         });
-        builder.addSubCommand("aliases", "dpm_admin", lang.get("help_aliases"), true, (p, args) -> {
+        builder.addSubCommand("aliases", "dpm.admin", lang.get("help_aliases"), true, (p, args) -> {
             if (args.length == 3) DPMFunction.setAliases((Player) p, args[1], args[2]);
             else p.sendMessage(prefix + lang.get("help_aliases"));
         });
-        builder.addSubCommand("list", "dpm_admin", lang.get("help_list"), true, (p, args) -> {
+        builder.addSubCommand("list", "dpm.admin", lang.get("help_list"), true, (p, args) -> {
             if (args.length == 1) {
                 p.sendMessage(prefix + lang.get("menu_list_title"));
                 plugin.menus.keySet().forEach(key -> {
@@ -62,14 +62,14 @@ public class DPMCommand {
                 });
             } else p.sendMessage(prefix + lang.get("help_list"));
         });
-        builder.addSubCommand("reload", "dpm_admin", lang.get("help_reload"), true, (p, args) -> {
+        builder.addSubCommand("reload", "dpm.admin", lang.get("help_reload"), true, (p, args) -> {
             if (args.length == 3) {
                 plugin.data.reload();
                 p.sendMessage(prefix + lang.get("reload_message"));
             } else p.sendMessage(prefix + lang.get("help_reload"));
         });
 
-        builder.addSubCommand("action", "dpm_admin", lang.get("help_action"), false, (p, args) -> {
+        builder.addSubCommand("action", "dpm.admin", lang.get("help_action"), false, (p, args) -> {
             if (args.length == 2) {
                 DPMFunction.openActionSettingGUI((Player) p, args[1]);
             } else p.sendMessage(prefix + lang.get("help_action"));
